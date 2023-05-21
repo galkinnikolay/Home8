7449b9e8417efb082b429261931a6aaf56a9472f
Nikolai Galkin <galkin_nikolay@icloud.com>
2023-05-21 19:24:21 +0300

    :qwe

---
+++ Zadacha47/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами от -10,0 до 10,0.
+
+// m = 3, n = 4.
+
+// 0,5 7,0 -2,0 -0,2
+
+// 1,0 -3,3 8,0 -9,9
+
+// 8,0 7,8 -7,1 9,0
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+double [,] array = new double [rows, columns];
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        array [i, j] = new Random().Next(-10, 10) / 10.0 ;
+    }
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
